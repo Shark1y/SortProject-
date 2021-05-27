8dd9b03b56be19235c77fe87e380f99db23114b9
Shark1y <84716479+Shark1y@users.noreply.github.com>
2021-05-27 12:43:14 -0400

    Update QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -10,6 +10,12 @@
 int partition(int array[], int first, int last);
 
 
+/*
+compare function, to compare the integers 
+if the numbers are equal, function returns 0
+if the number a is larger than number b, function returns 1 
+if the number a is smaller than number b, function returns -1
+*/
 int compare(int a, int b){
     if (a == b){
         return 0;
@@ -22,8 +28,15 @@ int compare(int a, int b){
 	}
 }
 
+/*
+"array" stands for the array we are working with 
+"first" stands for the first element of the array 
+"last" stands for the last element of the array 
+pivotIndex stands for the array element, that stands in a correct position even before sorting 
+*/
 void quickSort(int array[], int first, int last) {
 
+	//
 	if (last > first) {
 		int pivotIndex = partition(array, first, last);
 
@@ -33,25 +46,33 @@ void quickSort(int array[], int first, int last) {
 	}
 }
 
-
+/* 
+The function takes first element as pivot, change the pivot position to its correct position in sorted array, 
+and places all smaller elements to left of pivot, and all greater elements to right of the pivot 
+*/
 int partition(int array[], int first, int last) {
 
+	//start off with the pivot at the first element 
 	int pivot = array[first];
 
+	//variable for the forward search 
 	int low = first + 1;
 
+	//variable for the backward search 
 	int high = last;
 
 	while (high > low) {
 
+		//search forward 
 		while (low <= high && compare(array[low],pivot) <= 0){
 			low++;
 		}
-
+		//search backward 
 		while (low <= high && compare(array[high],pivot) > 0){
 			high--;
 		}
 
+		//swap elements in the array 
 		if (high > low) {
 
 			int temp = array[high];
@@ -66,6 +87,7 @@ int partition(int array[], int first, int last) {
 		high--;
 	}
 
+	//swap pivot with the highest element 
 	if (compare(pivot,array[high]) > 0) {
 
 		array[first] = array[high];
@@ -80,6 +102,7 @@ int partition(int array[], int first, int last) {
 	}
 }
 
+//function to initialize sorting 
 void sort(int array[],int length) {
 
 	quickSort(array, 0, length-1);
@@ -88,35 +111,44 @@ void sort(int array[],int length) {
 
 int main(int argc, char *argv[]){
 
-    //reading from a file
-    FILE* f = fopen (argv[1], "r");
-
-    //where to store the length of the array 
-    int length = 0;
-    //reading the length of the array 
-    fscanf (f, "%d", &length); 
-
-    //inputing values inside the array 
-    int temp;
-    int counter = 0;
-
-    int values[length];
-
-    while (!feof (f))
-    {  
-        fscanf (f, "%d", &temp);  
-        values[counter] = temp;   
-        counter++; 
-    }
-
-    fclose (f);
-
+     //starting the timer for the time consuming 
     clock_t begin = clock();
 
-	for (int i = 0; i < 1000; i++){
-	sort(values, length);
-	}
+    for (int i =0; i <1000; i++){
 
+        //reading from a file
+        FILE* f = fopen (argv[1], "r");
+
+        //where to store the length of the array 
+        int length = 0;
+        //reading the length of the array 
+        fscanf (f, "%d", &length); 
+
+        //inputing values inside the array 
+        int temp;
+
+        //counter is used to store the values one by one 
+        int counter = 0;
+
+        //initializing array for the values 
+        int values[length];
+
+        //input values untill we reach end of the file 
+        while (!feof (f))
+        {  
+            fscanf (f, "%d", &temp);  
+            values[counter] = temp;   
+            counter++; 
+        }
+
+        //closing the file we are working with 
+        fclose (f);
+
+        //call for sort function 
+        sort(values, length);
+    }
+    
+    //closing the timer for the time consuming 
     clock_t end = clock();
 
     printf("Spent: %f seconds\n", (double)(end - begin) / CLOCKS_PER_SEC);
